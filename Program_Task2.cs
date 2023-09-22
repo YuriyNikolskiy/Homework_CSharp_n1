f7ff0427c470ab1455c297a9d06210090d1cdb3a
Yuriy Nikolskiy <bankirerbe@gmail.com>
2023-09-23 00:37:22 +0300

    first commit

---
+++ Program_Task2.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter the first number: ");
+string number_1 = Console.ReadLine();
+Console.Write("Enter the second number: ");
+string number_2 = Console.ReadLine();
+int number1 = Int32.Parse(number_1);
+int number2 = Int32.Parse(number_2);
+
+if(number1>number2)
+{
+    Console.Write("Bigger number: ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("Bigger number: ");
+    Console.WriteLine(number2);
+}
